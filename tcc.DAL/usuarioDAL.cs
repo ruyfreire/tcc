2c738cf6f8b684ed2561d3714dff3b1ff2c51f5d
Ruy Freire <ruy.slv@gmail.com>
2018-08-24 23:57:29 -0300

    conexao com banco e metodo para criar novo usuario no banco

--- tcc.DAL/usuarioDAL.cs
+++ tcc.DAL/usuarioDAL.cs
@@ -5,13 +5,85 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using tcc.DTO;
 
 namespace tcc.DAL
 {
     public class UsuarioDAL
     {
 
-        public void buscaUsuario()
+        public int novoUsuarioDAL(Usuario novoUser)
+        {
+            try
+            {
+                /*Conexão com BD
+                * e busca todos os dados do usuario*/
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = Properties.Settings.Default.CST;
+                SqlCommand cm = new SqlCommand();
+                cm.CommandType = System.Data.CommandType.Text;
+
+                cm.CommandText = "INSERT INTO usuario (email, nome, senha, nascimento, sexo, peso, altura, objetivo)" +
+                                 "VALUES (@email, @nome, @senha, @nascimento, @sexo, @peso, @altura, @objetivo)";
+
+                /*Parametros irá substituir os valores dentro do campo */
+                cm.Parameters.Add("email", System.Data.SqlDbType.VarChar).Value = novoUser.email;
+                cm.Parameters.Add("nome", System.Data.SqlDbType.VarChar).Value = novoUser.nome;
+                cm.Parameters.Add("senha", System.Data.SqlDbType.Char).Value = novoUser.senha;
+                cm.Parameters.Add("nascimento", System.Data.SqlDbType.DateTime).Value = novoUser.nascimento;
+                cm.Parameters.Add("sexo", System.Data.SqlDbType.DateTime).Value = novoUser.sexo;
+                cm.Parameters.Add("peso", System.Data.SqlDbType.Real).Value = novoUser.peso;
+                cm.Parameters.Add("altura", System.Data.SqlDbType.Real).Value = novoUser.altura;
+                cm.Parameters.Add("objetivo", System.Data.SqlDbType.VarChar).Value = novoUser.objetivo;
+
+                cm.Connection = con;
+                con.Open();
+                int qtd = cm.ExecuteNonQuery();
+
+                return qtd;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /*
+        public void validaUser()
+        {
+            try
+            {
+
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = Properties.Settings.Default.CST;
+                SqlCommand cm = new SqlCommand();
+                cm.CommandType = System.Data.CommandType.Text;
+
+                cm.CommandText = "SELECT * FROM usuario WHERE email";
+
+                cm.Parameters.Add("email", System.Data.SqlDbType.VarChar).Value = novoUser.email;
+                cm.Parameters.Add("nome", System.Data.SqlDbType.VarChar).Value = novoUser.nome;
+                cm.Parameters.Add("senha", System.Data.SqlDbType.Char).Value = novoUser.senha;
+                cm.Parameters.Add("nascimento", System.Data.SqlDbType.DateTime).Value = novoUser.nascimento;
+                cm.Parameters.Add("sexo", System.Data.SqlDbType.DateTime).Value = novoUser.sexo;
+                cm.Parameters.Add("peso", System.Data.SqlDbType.Real).Value = novoUser.peso;
+                cm.Parameters.Add("altura", System.Data.SqlDbType.Real).Value = novoUser.altura;
+                cm.Parameters.Add("objetivo", System.Data.SqlDbType.VarChar).Value = novoUser.objetivo;
+
+                cm.Connection = con;
+                con.Open();
+                int qtd = cm.ExecuteNonQuery();
+
+                return qtd;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+         */
+
+        public void buscaUsuarioDAL()
         {
             try
             {
@@ -34,5 +106,8 @@ namespace tcc.DAL
                 throw ex;
             }
         }
+
+
+
     }
 }
