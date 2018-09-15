4b4a06b58bf0bc21900b5c7d43a5145005d5c803
Diego França <franca_00@hotmail.com>
2018-09-15 15:36:20 -0300

    CADASTRO DE DIETA

--- tcc/Form1.Designer.cs
+++ tcc/Form1.Designer.cs
@@ -28,12 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtQuantidadeProporcao = new System.Windows.Forms.TextBox();
+            this.txtTipoDeAlimento = new System.Windows.Forms.TextBox();
+            this.txtTipoDeRefeicao = new System.Windows.Forms.ComboBox();
+            this.txtDiaDaSemana = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(373, 407);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 25;
+            this.button1.Text = "CADASTRAR";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(351, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 24;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtQuantidadeProporcao
+            // 
+            this.txtQuantidadeProporcao.Location = new System.Drawing.Point(308, 357);
+            this.txtQuantidadeProporcao.Name = "txtQuantidadeProporcao";
+            this.txtQuantidadeProporcao.Size = new System.Drawing.Size(320, 20);
+            this.txtQuantidadeProporcao.TabIndex = 23;
+            // 
+            // txtTipoDeAlimento
+            // 
+            this.txtTipoDeAlimento.Location = new System.Drawing.Point(308, 315);
+            this.txtTipoDeAlimento.Name = "txtTipoDeAlimento";
+            this.txtTipoDeAlimento.Size = new System.Drawing.Size(320, 20);
+            this.txtTipoDeAlimento.TabIndex = 22;
+            // 
+            // txtTipoDeRefeicao
+            // 
+            this.txtTipoDeRefeicao.FormattingEnabled = true;
+            this.txtTipoDeRefeicao.Location = new System.Drawing.Point(308, 275);
+            this.txtTipoDeRefeicao.Name = "txtTipoDeRefeicao";
+            this.txtTipoDeRefeicao.Size = new System.Drawing.Size(320, 21);
+            this.txtTipoDeRefeicao.TabIndex = 20;
+            // 
+            // txtDiaDaSemana
+            // 
+            this.txtDiaDaSemana.FormattingEnabled = true;
+            this.txtDiaDaSemana.Items.AddRange(new object[] {
+            "Domingo",
+            "Segunda-feira",
+            "Terça-feira",
+            "Quarta-feira",
+            "Quinta-feira",
+            "Sexta-feira",
+            "Sábado"});
+            this.txtDiaDaSemana.Location = new System.Drawing.Point(308, 237);
+            this.txtDiaDaSemana.Name = "txtDiaDaSemana";
+            this.txtDiaDaSemana.Size = new System.Drawing.Size(320, 21);
+            this.txtDiaDaSemana.TabIndex = 19;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(136, 361);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(161, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "QUANTIDADE / PORÇÃO :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(165, 319);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(132, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "TIPO DE ALIMENTO :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(167, 280);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "TIPO DE REFEIÇÃO :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(185, 242);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "DIA DA SEMANA :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(347, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 17);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "C A D A S T R A R    D I E T A";
+            this.label1.UseCompatibleTextRendering = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtQuantidadeProporcao);
+            this.Controls.Add(this.txtTipoDeAlimento);
+            this.Controls.Add(this.txtTipoDeRefeicao);
+            this.Controls.Add(this.txtDiaDaSemana);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtQuantidadeProporcao;
+        private System.Windows.Forms.TextBox txtTipoDeAlimento;
+        private System.Windows.Forms.ComboBox txtTipoDeRefeicao;
+        private System.Windows.Forms.ComboBox txtDiaDaSemana;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
     }
 }
