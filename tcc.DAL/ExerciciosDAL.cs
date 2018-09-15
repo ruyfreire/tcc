fa931ad9147b423b2530e1b600d79a849d193324
Ruy Freire <ruy.slv@gmail.com>
2018-09-15 14:57:06 -0300

    inserindo treino no banco

--- tcc.DAL/ExerciciosDAL.cs
+++ tcc.DAL/ExerciciosDAL.cs
@@ -7,7 +7,107 @@ using tcc.DTO;
 namespace tcc.DAL
 {
     public class ExerciciosDAL
-    {   
+    {
+        /* Recebe id do treino e do exericio, e vincula os dois na tabela/dicionario treino_exericios 
+         retorna 1 se inserir corretamente,
+         retorna 0 se falhar */
+        public int incluiExercicioTreino(int id_treino, int id_exercicio)
+        {
+            try
+            {
+                if (existeLinkExercicioTreino(id_treino, id_exercicio) == 1)
+                {
+                    return -1;
+                }
+
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = Properties.Settings.Default.CST;
+                SqlCommand cm = new SqlCommand();
+                cm.CommandType = System.Data.CommandType.Text;
+
+                cm.CommandText = "INSERT INTO treino_exericio (link_treino, link_exericio) " +
+                    "VALUES (@link_treino, @link_exericio)";
+
+                //Parametros irá substituir os valores dentro do campo
+                cm.Parameters.Add("link_treino", System.Data.SqlDbType.Int).Value = id_treino;
+                cm.Parameters.Add("link_exericio", System.Data.SqlDbType.Int).Value = id_exercicio;
+
+                cm.Connection = con;
+                con.Open();
+
+                int qtd = cm.ExecuteNonQuery();
+
+                if (qtd > 0) return 1;
+                else return 0;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /* Recebe id do treino e do exercicio, e exclui o vinculo na tabela/dicionario treino_exercicio 
+         retorna 1 se excluir corretamente,
+         retorna 0 se falhar */
+        public int excluiExercicioTreino(int id_treino, int id_exercicio)
+        {
+            try
+            {
+                if (existeLinkExercicioTreino(id_treino, id_exercicio) == 0)
+                {
+                    return -1;
+                }
+
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = Properties.Settings.Default.CST;
+                SqlCommand cm = new SqlCommand();
+                cm.CommandType = System.Data.CommandType.Text;
+
+                cm.CommandText = "DELETE FROM treino_exercicio WHERE link_treino=" + id_treino + " AND " + "link_exericio=" + id_exercicio;
+
+                cm.Connection = con;
+                con.Open();
+
+                int qtd = cm.ExecuteNonQuery();
+
+                if (qtd > 0) return 1;
+                else return 0;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /* Verifica na tabela/dicionario treino_exercicio se possui o link do treino e exercicio informado,
+         retorna 1 se encontrar,
+         retorna 0 se não encontrar */
+        public int existeLinkExercicioTreino(int id_treino, int id_exercicio)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection();
+                con.ConnectionString = Properties.Settings.Default.CST;
+                SqlCommand cm = new SqlCommand();
+                cm.CommandType = System.Data.CommandType.Text;
+                SqlDataReader er;
+
+                cm.CommandText = "SELECT * FROM treino_exericicio WHERE link_treino=" + id_treino + " AND " + "link_exercicio=" + id_exercicio;
+
+                cm.Connection = con;
+                con.Open();
+
+                er = cm.ExecuteReader();
+                if (er.HasRows) return 1;
+                else return 0;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
         /* Busca um exercicio especifico,
          de acordo com o id que é passado ao método */
         public Exercicio buscaExercicioEspecifico(int id)
