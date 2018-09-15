7de6f994d924ce55e9f3337c97aeb275f4eba446
Ana Carla <Ana Carla@ANACARLA-PC>
2018-09-15 12:58:41 -0300

    Ana

--- tcc/Form1.Designer.cs
+++ tcc/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace tcc
 {
-    partial class exercicio
+    partial class Form1
     {
         /// <summary>
         /// Required designer variable.
@@ -28,32 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(164, 130);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(109, 60);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // exercicio
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(446, 276);
-            this.Controls.Add(this.button1);
-            this.Name = "exercicio";
-            this.Text = "exercicio";
-            this.ResumeLayout(false);
-
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Text = "Form1";
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
